17d8580e13ef581baa62dd9991b3db28aa413b9d
RostSLO <63428412+RostSLO@users.noreply.github.com>
2023-12-30 13:14:33 -0700

    Added writhing and reading to/from JSON file

--- Reaserch.py
+++ Reaserch.py
@@ -1,15 +1,50 @@
 import http.client
+import json
+import os
 
-conn = http.client.HTTPSConnection("moviesdatabase.p.rapidapi.com")
 
-headers = {
-    'X-RapidAPI-Key': "8bf0871ea3msh8bbb7d0dd036a04p15f9cajsn0e547477cf42",
-    'X-RapidAPI-Host': "moviesdatabase.p.rapidapi.com"
-}
+def finderCall():
+    conn = http.client.HTTPSConnection("moviesdatabase.p.rapidapi.com")
 
-conn.request("GET", "/titles/search/title/Iron%20Man?exact=false&info=base_info&titleType=movie", headers=headers)
+    headers = {
+        'X-RapidAPI-Key': "8bf0871ea3msh8bbb7d0dd036a04p15f9cajsn0e547477cf42",
+        'X-RapidAPI-Host': "moviesdatabase.p.rapidapi.com"
+    }
 
-res = conn.getresponse()
-data = res.read()
+    conn.request("GET", "/titles/search/title/Iron%20Man?exact=true&info=base_info&titleType=movie", headers=headers)
 
-print(data.decode("utf-8"))
+    res = conn.getresponse()
+    data = res.read()
+
+    with open(os.path.dirname(__file__) + '\dataFromURL.json','w') as f_json:    
+        f_json.write(data.decode("utf-8"))
+        f_json.write("\n")
+    f_json.close()
+
+
+def getValuesFromJSON():
+
+    # Opening JSON file
+    # UPDATE the abs path using os.path.abspath("data.json")
+    f = open(os.path.dirname(__file__) + ("\dataFromURL.json"))
+    
+    # returns JSON object as 
+    # a dictionary
+    dataJSON = json.load(f)
+    
+    # Iterating through the json
+    # list
+    for i in dataJSON['results']:
+        print(i["primaryImage"]["id"] + "\n")
+
+    # Closing file
+    f.close()
+
+
+
+#finderCall()
+getValuesFromJSON()
+
+print(os.getcwd())
+
+
