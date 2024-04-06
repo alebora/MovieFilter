b959ed754878d8dcf1f981246eada3de4cb004ef
alebora <148511093+alebora@users.noreply.github.com>
2024-04-06 15:35:27 -0400

    clean

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def search_post():
             return render_template("second.html", jsonObj = "", start = 0)
     else:
         return render_template("second.html", jsonObj = "", start = 0)
-       
+          
 def finderCall(str, select):
     conn = http.client.HTTPSConnection("moviesdatabase.p.rapidapi.com")
    
