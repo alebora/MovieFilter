156fe4ff8b292b28ae87f8bc2c1738dbbb102843
alebora <148511093+alebora@users.noreply.github.com>
2024-07-12 08:44:03 -0400

    clean

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def search():
 @app.route("/search", methods=['POST'])            
 def search_post():          
     str = request.form['text']     
-    select = request.form['selection']              
+    select = request.form['selection']                
     if str != "":         
         result = finderCall(str, select)
         result = [item for item in result["results"] if item['ratingsSummary']['voteCount'] > 100000]
