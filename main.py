d5e57483e8f38bf7815e758268c88008fa969457
alebora <148511093+alebora@users.noreply.github.com>
2024-07-15 09:53:45 -0400

    clean

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def search():
     return render_template("second.html", jsonObj = "", start = 1)                
                                                                                                                                                            
 @app.route("/search", methods=['POST'])            
-def search_post():          
+def search_post():              
     str = request.form['text']     
     select = request.form['selection']                   
     if str != "":         
