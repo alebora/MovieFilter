164d116fd2949759ceb00f56e3c20dbf72bede70
alebora <148511093+alebora@users.noreply.github.com>
2024-08-15 08:52:23 -0400

    clean

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__)        
 searchDict = {}      
    
-@app.route("/", methods=['GET', 'POST'] )                  
+@app.route("/", methods=['GET', 'POST'] )                     
 def root():               
     return render_template("index.html")                
                                 
